77877d4c5e0fabfa320c66cfb2fd15be2c3765cd
yuanluyan521 <45061579+yuanluyan521@users.noreply.github.com>
2019-07-12 08:59:19 +0800

    712

---
+++ 7.10.php
@@ -0,0 +1,51 @@
+<?php
+
+// echo strrev("ABC"); 
+
+// function two($str,$needle){
+
+//     $len = strlen($str);
+//     $num = 0;
+
+//     for ($i=0; $i <$len; $i++) { 
+//         if($str[$i]==$needle){
+//             $num++;
+//         }
+//     }
+//     return $sum;
+// }
+ // $sum=0;
+
+ // for ($i=1; $i <100 ; $i++) { 
+ //     $sum+=$i; 
+ // }
+
+ // echo $sum;
+ // 
+
+$s='Hello World'; 
+$s=strtolower($s); 
+$s=preg_replace('/\s/','',$s); 
+echo $s
+
+
+  // public function day6(){
+  //       $n = 100;
+  //       $m = 1300;
+  //       $num = 0;
+  //       $list = [];
+  //       for($n;$n<=$m;$n++){
+  //           $arr = str_split($n,1);
+  //           foreach($arr as $k => $v){
+  //               if($v == 1){
+  //                   $num++;
+  //                   $list[] = $n;
+  //               }
+  //           }
+  //       }
+  //       echo $num;
+  //   }
+
+
+
+?>
